67e8ebbee691f3581d8d5b21fcb9599ed7d4ff1b
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2025-04-15 15:41:38 +0000

    Caught Up

--- katas-3.js
+++ katas-3.js
@@ -18,12 +18,12 @@
  * Display gatekeeper name and keymaster name for this kata
  * @param = none
  * @return = none
- */
+ * /
 
 /* Function addStudents(count)
- * Create a students array, nothing in it. 
- * Use a for loop to add student names to the students array.
+ * Use a for loop to add student names to the global students array.
  * The loop should run count number of times. 
+ * The loop should ask the user for a name and push it to the students array.
  * Return the array of student names 
  * @param = count {integer}
  * @return = none
