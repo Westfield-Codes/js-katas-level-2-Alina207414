d01efc2048ed6366582b6371165cdeab35a04d68
Alina207414 <alina207414@student-schoolsofwestfield.org>
2025-05-13 14:55:48 +0000

    make list working

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-var studentFlavors = []
+// Every day, you are sent to buy 5 items. 
 
-function askFlavor() {
-let name = prompt("what your name?");
-let flavor = prompt("What your favorite flavor?");
-let pair = [name,flavor];
-return pair;
-}
-
-function main(){
-    studentFlavors.push(askFlavor());
-    if (confirm("more")) studentFlavors.push(askFlavor());
-    alert(studentFlavors.toString());
-    let name = prompt("Which student?");
-}
-
-function goodName(test) {
-    for (let name = 0; name < studentFlavors.length;name++) {
-        if (studentFlavors[name][0] == test);
-            return true;
-
+/* 1. for loop and string concatenation */
+ function main(){
+    makeList();
+ }
+/* Function makeList()
+ * Create a shopping list string called shoppingList as "To Buy:"
+ * create a five-item loop, and in the loop, 
+ * ask for next item to buy (item #), and add it to the list 
+ * add a line break after each item added
+ * when loop's done, alert the list
+ */
+function makeList() {
+    let shoppingList = "to buy: \n";
+    for (let item = 1; item <= 5; item++){ 
+        shoppingList+=prompt(" item " + item + "\n");
+           alert(shoppingList);
+        }   
     }
-}
+ 
+/* 2. for loop and array with toString */
+
+/* Same function, using previous code, BUT:
+ * build shopArray variable for the shopping list
+ * adjust loop to work with the array index values (start at 0)
+ * ask for next list item and push it to the shopArray
+ * no need to add a line break after
+ * at end of loop, alert the list title, followed by
+ * the list with toString() method (it won't have line breaks)*/
+
+/* 3. for loop and array with join method 
+
+/* Same function, using previous code, BUT:
+ * the at end, alert list with array.join() method with line breaks 
+ * instead of commas */
+ 
+/* 4. loops, arrays and functions */
+
+/* Same function, using previous code, but THIS TIME:
+ * store each item as a numbered item ["1. cookies","2. donuts"])   
+ * using the loop index. */
